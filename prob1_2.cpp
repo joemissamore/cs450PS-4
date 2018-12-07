3fcf4b88abef22d25b3dc0d0a9a6c0cb7a6c61fc
joemissamore <joemissamore@live.com>
2018-12-06 18:40:42 -0800

    before getting CLOCK correct

--- prob1_2.cpp
+++ prob1_2.cpp
@@ -3,6 +3,8 @@
 #include <vector>
 #include <bitset>
 #include <math.h>
+#define DEBUG 1
+#define debug if (DEBUG) cout
 
 using namespace std;
 
@@ -21,6 +23,53 @@ struct VirtualPageInfo
     int numByesInPage;
 };
 
+void PrintVector(vector<VirtualPage> v)
+{
+    for (int i = 0; i < v.size(); i++)
+    {
+        debug << "------------------" << endl;
+        debug << "pos #: " << i << endl;
+        debug << "-------" << endl;
+        debug << "valid: " << v[i].valid << endl;
+        debug << "permission: " << v[i].permission << endl;
+        debug << "ppn: " << v[i].ppn << endl;
+        debug << "useBit: " << v[i].useBit << endl;
+        debug << "------------------" << endl;
+    }
+}
+
+
+int CalculatePhysicalAddress(int ppn, int moveIndex, int hexVal, const VirtualPageInfo &virtualPageInfo)
+{
+    /* Calculates the index for looking up in the virtual page table */ 
+    // int bitIndex = virtualPageInfo.numBitsPhysAdd - virtualPageInfo.numBitsVirtAdd + 1; // 2
+    // int shift = virtualPageInfo.numBitsVirtAdd - bitIndex; // 5
+
+    int tagBits = virtualPageInfo.numBitsVirtAdd - moveIndex; // 3
+    int insertIntoPhysicalAddress = (ppn << moveIndex);
+    /* insertIntoPhysicalAddress = 192 = 11000000
+     * all we care about are the first 3 bits = 110
+     * 8 bits */ 
+
+    /* Zero's out bits 
+     * zero's out the first 2 
+     * bits in the entered hex 
+     * value. */
+    for (int n = moveIndex + 1; n < virtualPageInfo.numBitsVirtAdd - 1; n++)
+    {
+        hexVal &= ~(1 << n);
+    }
+    /* hexVal = 10 = 0001010 
+     * these first three bits
+     * will be masked with the
+     * first 3 bits of insertIntoPhysicalAddress */
+
+    int physicalAddress = (hexVal | insertIntoPhysicalAddress);
+    /* physicalAddress = 202 = 11001010 = 0xCA
+     * 8 bits */
+    return physicalAddress;
+}
+
 int main(int argc, char ** argv)
 {
     if (argc < 2)
@@ -29,27 +78,26 @@ int main(int argc, char ** argv)
         exit(1);
     }
 
+    /* Reading in values from file */
     int temp = 0;
-    int addressInfo[3] = {0};
-    vector <int> fileInfo;
+
     vector <VirtualPage> virtualPages;
     ifstream file; 
     file.open(argv[1]);
     /* [0] = # of bits in virtual address
      * [1] = # of bits in physical address
      * [2] = # of BYTES in a page */
+
+    /* Get virtual page information */
     VirtualPageInfo virtualPageInfo;
-    for (int i = 0; i < 3; i ++)
-    {
-        file >> temp;
-        if (i % 3 == 0)
-            virtualPageInfo.numBitsVirtAdd = temp;
-        else if (i % 3 == 1)
-            virtualPageInfo.numBitsPhysAdd = temp;
-        else
-            virtualPageInfo.numByesInPage = temp;
-    }
+    file >> temp;
+    virtualPageInfo.numBitsVirtAdd = temp;
+    file >> temp;
+    virtualPageInfo.numBitsPhysAdd = temp;
+    file >> temp;
+    virtualPageInfo.numByesInPage = temp;
     
+    /* Get virtual page information */ 
     int numOfVirtualPages = 0; // 4
     while (file >> temp)
     {
@@ -66,12 +114,15 @@ int main(int argc, char ** argv)
 
         numOfVirtualPages++;
     }
+    file.close();
 
 
-    int hexVal = 0;
-    int bitIndex = virtualPageInfo.numBitsPhysAdd - virtualPageInfo.numBitsVirtAdd + 1; // 2
+    debug << "numOfVirtualPages: " << numOfVirtualPages << endl;
+
+    int clockAlgo = 0;
     int moveIndex = log2(virtualPageInfo.numByesInPage); // 5
     double moveIndexCheck = log2(virtualPageInfo.numByesInPage);
+    /* ppn = 6 */
 
     /* Checking to make sure log2 returned an integer value */
     if (moveIndex != moveIndexCheck)
@@ -80,75 +131,104 @@ int main(int argc, char ** argv)
         exit(1);
     }
 
-    cout << "Enter hex address: ";
-    cin >> hex >> hexVal;
-    
-    /* hexVal = 0x4A = 1001010 
-     * moveIndex = 5 = 101 */
-    int indexVirt = (hexVal >> moveIndex);
-    /* indexVirt = 2 */
-    int ppn = virtualPages[indexVirt].ppn;
-    /* ppn = 6 */
-
-    int insertIntoPhysicalAddress = (ppn << moveIndex);
-    /* insertIntoPhysicalAddress = 192 = 11000000
-     * all we care about are the first 3 bits = 110
-     * 8 bits */ 
-
-    /* Zero's out bits 
-     * zero's out the first 2 
-     * bits in the entered hex 
-     * value. */
-    for (int n = virtualPageInfo.numBitsVirtAdd - 1; n < virtualPageInfo.numBitsVirtAdd + bitIndex - 1; n++)
+    while (true)
     {
-        hexVal &= ~(1 << n);
-    }
-    /* hexVal = 10 = 0001010 
-     * these first three bits
-     * will be masked with the
-     * first 3 bits of insertIntoPhysicalAddress */
-
-    int physicalAddress = (hexVal | insertIntoPhysicalAddress);
-    /* physicalAddress = 202 = 11001010 = 0xCA
-     * 8 bits */
-    
-    // When pages are not in physical memory but have a permission bit set to 1, print DISK.
-    if (virtualPages[indexVirt].valid == 0 && virtualPages[indexVirt].permission == 1)
-    {
-        #ifdef PROB1
-        cout << "DISK" << endl;
-        #else
-
-        /* CLOCK ALGO */ 
-        for (int i = 0; 1; i++)
-        {
-            if (virtualPages[i].valid == 0
-                && virtualPages[i].permission == 1
-                && virtualPages[i].useBit == 0)
-            {
-                virtualPages[i].valid = 1;
-                virtualPages[i].useBit = 1;
-                cout << "PAGE FAULT"<< endl;
-                printf("0x%X\n", physicalAddress);
-                break;
-            }
-            else 
-                virtualPages[i].useBit = 0;
-
-            if (i == numOfVirtualPages - 1)
-                i = 0;
-        }
-        #endif
-
-    }
-
+        int hexVal = 0;
+        cout << ">>> ";
+        cin >> hex >> hexVal;
         
-    else if (virtualPages[indexVirt].valid == 0)
-        cout << "SEGFAULT" << endl;
-    else
-        printf("0x%X\n", physicalAddress);
+        
+        /* Gets the virtual index
+        * hexVal = 0x4A = 1001010 
+        * moveIndex = 5 = 101 */
+        int indexVirt = (hexVal >> moveIndex);
+        /* indexVirt = 2 */
+        int ppn = virtualPages[indexVirt].ppn;
+
+        bool segfault = false;
+        bool disk = false;
 
 
-    file.close();
+        // When pages are not in physical memory but have a permission bit set to 1, print DISK.
+        /* Get PPN value */
+        if (virtualPages[indexVirt].valid == 0 && virtualPages[indexVirt].permission == 1)
+        {
+            #ifdef PROB1
+            disk = true;
+            #else
+            disk = true;
+            /* CLOCK ALGO */ 
+            // debug << "executing clock ago..." << endl;
+            // debug << "executing clock algo on idx: " << clockAlgo << endl;
+            for (; ; clockAlgo++)
+            {
+                if (clockAlgo > numOfVirtualPages)
+                {
+                    debug << "setting clockAlgo to 0" << endl;
+                    clockAlgo = 0;
+                }   
+
+                if (virtualPages[clockAlgo].permission == 1
+                    && virtualPages[clockAlgo].useBit == 0)
+                {
+                    debug << "executing clock algo on idx: " << clockAlgo << endl;
+                    /* Map to DISK */
+                    virtualPages[clockAlgo].valid = 0;
+                    virtualPages[clockAlgo].useBit = 0;
+
+                    /* Set virtual page table index */
+                    virtualPages[indexVirt].ppn = virtualPages[clockAlgo].ppn;
+                    /* Validate the virtual page table entry */
+                    virtualPages[indexVirt].valid = 1;
+                    /* Just in case permissions was previously set to 0,
+                    * now we have permission to access ppn. */
+                    virtualPages[indexVirt].permission = 1;
+                    virtualPages[indexVirt].useBit = 1;
+                    break;
+                }
+                else 
+                    virtualPages[clockAlgo].useBit = 0;
+            }
+            debug << "clock algo position (right after for loop): " << clockAlgo << endl;
+
+            /* After the clock algo finishes, we must advance its position. 
+            * If the clock algo is at the last position move its position
+            * to the first element. */
+            if (clockAlgo == numOfVirtualPages - 1)    
+                clockAlgo = 0;
+            else
+                clockAlgo++;
+            #endif
+
+        }
+        else if (virtualPages[indexVirt].permission == 0)
+            segfault = true;
+
+        if (segfault)
+            cout << "SEGFAULT" << endl;
+    #ifdef PROB1
+        else if (disk)
+        {
+            cout << "DISK" << endl;
+        }
+    #else
+        else if (disk)
+        {
+            cout << "PAGE FAULT" << endl;
+            printf("0x%X\n", CalculatePhysicalAddress(ppn, moveIndex, hexVal, virtualPageInfo));
+        }
+    #endif
+        else
+        {
+            printf("0x%X\n", CalculatePhysicalAddress(ppn, moveIndex, hexVal, virtualPageInfo));
+            virtualPages[indexVirt].useBit = 1;
+        }
+
+        debug << endl << "Debugging Info: " << endl;
+        PrintVector(virtualPages);
+        debug << "clock algo position: " << clockAlgo << endl;
+        debug << "indexVirt: " << indexVirt << endl;
+    }
+
     return 0;
 }
